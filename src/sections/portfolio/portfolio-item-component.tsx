7b5e9ffab81db755cd5336ebd04dd75f10f3a4e2
Dibo Gonda <dibolisa@web.de>
2023-03-02 19:21:57 +0100

    Introduced portfolio item component

---
+++ src/sections/portfolio/portfolio-item-component.tsx
@@ -0,0 +1,70 @@
+import InfoPanelComponent from "../../utils/info-panel/info-panel-component";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import React from "react";
+import SocialLinkComponent from "../../components/social/social-link-component";
+import "./portfolio-item-component.css";
+
+interface PortfolioItemComponent {
+    title: string;
+    imgPath: string;
+    url?: string;
+    gitHubUrl: string;
+}
+
+const PortfolioItemComponent = (props: PortfolioItemComponent) => {
+    return(
+        <div style={{width: "60%", gap: "1rem"}} className={"flex-column"}>
+            <InfoPanelComponent>
+                {/*Render Preview here!*/}
+                <img src={require("../../simutales.png")}
+                     style={{width: "100%", borderRadius: "0.8rem", border: "1px white solid"}}></img>
+            </InfoPanelComponent>
+
+            <InfoPanelComponent>
+                <div className={"flex-row"} style={{color: "rgb(92, 187, 191)"}}>
+                    <div style={{width: "33%"}}></div>
+                    <div style={{fontWeight: "bold", alignSelf: "center", width: "34%"}}>{props.title}</div>
+                    <div style={{width: "33%", justifyContent: "flex-end", gap: "0.5rem", alignSelf: "center"}}
+                         className={"flex-row"}>
+
+                        {props.url &&
+                            <SocialLinkComponent link={props.url}>
+                                <OpenInNewIcon className={"link-icon"}></OpenInNewIcon>
+                            </SocialLinkComponent>
+                        }
+
+                        <SocialLinkComponent link={props.gitHubUrl}>
+                            <GitHubIcon className={"link-icon"}></GitHubIcon>
+                        </SocialLinkComponent>
+                    </div>
+                </div>
+
+
+                <div style={{textAlign: "left"}}>
+                    Eine multiplayer Lebenssimulation in der die Freiheit hast, das zu tun was du möchtest!
+                    Baue ein Haus, oder auch eine ganze Stadt, erkunde, eröffne deinen eigenen Laden, oder finde die
+                    große Liebe!
+                    <br></br>
+                    <br></br>
+                    Tauche in eine aufregende Welt voller Möglichkeiten ein und erlebe das Leben in einer lebendigen
+                    vollständig simulierten Nachbarschaft.
+                    <br></br>
+                    <br></br>
+                    Spiele mit Freunden und gestalte mit ihnen gemeinsam die Stadt oder lasst eure Simus miteinander
+                    interagieren!
+
+                </div>
+
+                {/*<div><span style={{color: "rgb(92, 187, 191)"}}>&gt;</span> Unity/C#</div>*/}
+                <div>
+                    #GameDev #Unity|C#
+                </div>
+
+            </InfoPanelComponent>
+        </div>
+
+    );
+}
+
+export default PortfolioItemComponent;
--- src/sections/portfolio/portfolio-section-component.tsx
+++ src/sections/portfolio/portfolio-section-component.tsx
@@ -4,6 +4,7 @@ import "../../App.css";
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import SectionTitleComponent from "../../utils/section-title-component";
+import PortfolioItemComponent from "./portfolio-item-component";
 
 interface PortfolioComponentProps {
 
@@ -14,49 +15,7 @@ const PortfolioSectionComponent = (props: PortfolioComponentProps) => {
     return (
         <section id="portfolio" style={{alignItems: "center"}} className={"section-container flex-column"}>
             <SectionTitleComponent title="Portfolio"></SectionTitleComponent>
-
-            <div style={{width: "60%", gap: "1rem"}} className={"flex-column"}>
-                <InfoPanelComponent>
-                    {/*Render Preview here!*/}
-                    <img src={require("../../simutales.png")}
-                         style={{width: "100%", borderRadius: "0.8rem", border: "1px white solid"}}></img>
-                </InfoPanelComponent>
-
-                <InfoPanelComponent>
-                    <div className={"flex-row"} style={{color: "rgb(92, 187, 191)"}}>
-                        <div style={{width: "33%"}}></div>
-                        <div style={{fontWeight: "bold", alignSelf: "center", width: "34%"}}>Simutales</div>
-                        <div style={{width: "33%", justifyContent: "flex-end", gap: "0.5rem", alignSelf: "center"}}
-                             className={"flex-row"}>
-                            <OpenInNewIcon style={{fontSize: "1.2rem"}}></OpenInNewIcon>
-                            <GitHubIcon style={{fontSize: "1.2rem"}}></GitHubIcon>
-                        </div>
-                    </div>
-
-
-                    <div style={{textAlign: "left"}}>
-                        Eine multiplayer Lebenssimulation in der die Freiheit hast, das zu tun was du möchtest!
-                        Baue ein Haus, oder auch eine ganze Stadt, erkunde, eröffne deinen eigenen Laden, oder finde die
-                        große Liebe!
-                        <br></br>
-                        <br></br>
-                        Tauche in eine aufregende Welt voller Möglichkeiten ein und erlebe das Leben in einer lebendigen
-                        vollständig simulierten Nachbarschaft.
-                        <br></br>
-                        <br></br>
-                        Spiele mit Freunden und gestalte mit ihnen gemeinsam die Stadt oder lasst eure Simus miteinander
-                        interagieren!
-
-                    </div>
-
-                    {/*<div><span style={{color: "rgb(92, 187, 191)"}}>&gt;</span> Unity/C#</div>*/}
-                    <div>
-                        #GameDev #Unity|C#
-                    </div>
-
-                </InfoPanelComponent>
-            </div>
-
+            <PortfolioItemComponent title="Simutales" imgPath={"../../simutales.png"} url={"https://themoonforge.github.io/simutales.github.io/"} gitHubUrl={"https://github.com/themoonforge/simutales"}></PortfolioItemComponent>
         </section>
     );
 
